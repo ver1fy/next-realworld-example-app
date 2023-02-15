a4154049009e08121cf9773a3703e96f291bae98
Tom <tom-1@mail.com>
2023-02-15 23:13:24 +0000

    chore: added global CSS and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,37 @@
-import './globals.css'
+import {
+  Merriweather_Sans,
+  Source_Sans_Pro,
+  Source_Serif_Pro,
+  Titillium_Web,
+} from "@next/font/google";
+import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const titillium = Titillium_Web({
+  weight: "700",
+  subsets: ["latin"],
+});
+
+const sourceSerifPro = Source_Serif_Pro({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+const merriweatherSans = Merriweather_Sans({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+const sourceSansPro = Source_Sans_Pro({
+  weight: ["300", "400", "600", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
+
+const fonts = `${titillium.className} ${sourceSerifPro.className} ${merriweatherSans.className} ${sourceSansPro.className}`;
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
+    <html lang="en" className={fonts}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
@@ -14,5 +39,7 @@ export default function RootLayout({
       <head />
       <body>{children}</body>
     </html>
-  )
-}
+  );
+};
+
+export default RootLayout;
